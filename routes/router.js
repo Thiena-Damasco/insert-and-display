b719fff906dffce35119ab4f7a392120ecddccc2
Thiena-Damasco <thiena8@gmail.com>
2024-09-26 09:48:22 +0800

    Delete routes directory

--- routes/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const nodejs = require('./controller/Controller.js')
-const routes = express.Router();
-
-routes.get('/', nodejs.index);
-routes.post('/save', nodejs.save);
-
-module.exports = routes;
